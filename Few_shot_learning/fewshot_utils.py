f8c973907d80e270efbb19ce5c62b7e8adf8b706
Eileen Wang <eileen@Eileens-MacBook-Air.local>
2023-10-31 14:09:30 -0400

    fixing merced

--- Few_shot_learning/fewshot_utils.py
+++ Few_shot_learning/fewshot_utils.py
@@ -1,3 +1,11 @@
+from typing import List, Tuple
+
+from matplotlib import pyplot as plt
+from torch import Tensor, nn
+import numpy as np
+import torchvision
+
+
 def sliding_average(value_list: List[float], window: int) -> float:
     """
     Computes the average of the latest instances in a list
@@ -15,3 +23,17 @@ def sliding_average(value_list: List[float], window: int) -> float:
     if len(value_list) == 0:
         raise ValueError("Cannot perform sliding average on an empty list.")
     return np.asarray(value_list[-window:]).mean()
+
+def plot_images(images: Tensor, title: str, images_per_row: int):
+    """
+    Plot images in a grid.
+    Args:
+        images: 4D mini-batch Tensor of shape (B x C x H x W)
+        title: title of the figure to plot
+        images_per_row: number of images in each row of the grid
+    """
+    plt.figure()
+    plt.title(title)
+    plt.imshow(
+        torchvision.utils.make_grid(images, nrow=images_per_row).permute(1, 2, 0)
+    )
