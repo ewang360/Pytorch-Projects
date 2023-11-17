456a5719874caf44e9b6c24f71267836633b8c26
ewang360 <elw96@pitt.edu>
2023-11-16 21:41:11 -0500

    got image buttons working

--- Few_shot_learning/tile_gui.py
+++ Few_shot_learning/tile_gui.py
@@ -1,5 +1,7 @@
 # Import Module
 from tkinter import *
+from tkinter.ttk import *
+import numpy as np
 
 # create root window
 root = Tk()
@@ -9,21 +11,31 @@ root.title("Welcome to GeekForGeeks")
 # Set geometry(widthxheight)
 root.geometry('350x200')
 
-# adding a label to the root window
-lbl = Label(root, text = "Are you a Geek?")
-lbl.grid()
+# Adding widgets to the root window 
+Label(root, text = 'GeeksforGeeks', font =( 
+  'Verdana', 15)).pack(side = TOP, pady = 10) 
+  
+# Creating a photoimage object to use image 
+# photos = np.empty(5)
+# print(photos.__sizeof__)
 
-# function to display text when
-# button is clicked
-def clicked():
-	lbl.configure(text = "I just got clicked")
+b = []
+p = []
 
-# button widget with red color text
-# inside
-btn = Button(root, text = "Click me" ,
-			fg = "red", command=clicked)
-# set Button grid
-btn.grid(column=1, row=0)
+for i in range(2):
+	for j in range(2):
+		name = "output/River_" + str(i) + "_" + str(j) + ".png"
+		photo = PhotoImage(file = name)
+		p.append(photo.subsample(2,2))
+		photoimage = p[i*2+j]
+
+		# here, image option is used to 
+		# set image on button 
+		# button.append(Button(root, text = 'Click Me !', image = photoimage).pack(side = TOP))
+		button = Button(root, text = 'Click Me !', image = photoimage)
+		button.pack()
+		b.append(button)
+
 
 # Execute Tkinter
 root.mainloop()
